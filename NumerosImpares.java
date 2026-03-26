6ca8788a0541b36f774419c9a520aff48edf4d3a
Jessica Yareny Amezcua Razo <yareny0099_@outlook.es>
2026-03-26 12:50:25 -0600

    Add files via upload

---
+++ NumerosImpares.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class NumerosImpares {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese un número entero positivo: ");
+        int numero = sc.nextInt();
+        int i = 1;
+        System.out.println("Números impares :");
+        do {
+            if (i % 2 != 0) {
+                System.out.println(i);  
+            }
+            i++;
+        } while (i < numero);
+        sc.close();
+
+ 
+    }
+    
+}
